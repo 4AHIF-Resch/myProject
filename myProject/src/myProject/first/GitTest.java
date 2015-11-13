242b4c2a560fe95c7558e160fb81f6f20e35a882
resch <resch@spengergasse.at>
2015-11-13 17:01:31 +0100

    changed output message

--- myProject/src/myProject/first/GitTest.java
+++ myProject/src/myProject/first/GitTest.java
@@ -2,6 +2,6 @@ package myProject.first;
 
 public class GitTest {
 	  public static void main(String[] args) {
-		    System.out.println("Git is fun");
+		    System.out.println("Git is cool");
 		  }
 }
