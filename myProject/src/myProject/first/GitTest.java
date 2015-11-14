eca8212ed9b27c99f9496f9a572e5d058fc46214
resch <resch@spengergasse.at>
2015-11-14 15:10:41 +0100

    Changed code for "firstBranch"

--- myProject/src/myProject/first/GitTest.java
+++ myProject/src/myProject/first/GitTest.java
@@ -3,5 +3,6 @@ package myProject.first;
 public class GitTest {
 	  public static void main(String[] args) {
 		    System.out.println("Git is cool");
+		    // no new ideas, but created a new branch :-)
 		  }
 }
